06384139be9652efc37aee9156ecb6d5148f4d6b
Minira Samadova <samadova.minira@gmail.com>
2020-01-31 22:12:31 +0900

    added schema to app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
+//express-graphql module allows express to understand graphql queries
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
 const app = express();
+
+//graphql middleware
+//if API request is made to the following enpoint
+//the handling will be passed onto graphqlHTTP
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
 
 app.listen(4000, () => {
   console.log('App listening on port 4000');
