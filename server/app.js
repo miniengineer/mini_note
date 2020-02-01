434b040d50106f5a1db1b879237d922300ede220
Minira Samadova <samadova.minira@gmail.com>
2020-02-01 16:59:01 +0900

    added body parser, refactored graphqlHttp middleware

--- server/app.js
+++ server/app.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const app = express();
 
 //express-graphql module allows express to understand graphql queries
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema');
-
-const app = express();
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 //graphql middleware
 //if API request is made to the following enpoint
 //the handling will be passed onto graphqlHTTP
-app.use('/graphql', graphqlHTTP({
-  schema,
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+
+    }
+
+    type RootMutation {
+
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {},
   graphiql: true
 }));
+
+app.use(bodyParser.json());
 
 app.listen(4000, () => {
   console.log('App listening on port 4000');
